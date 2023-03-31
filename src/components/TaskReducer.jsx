406c13c85e9deeff11f724660756e485d3101002
zeff96 <zeffo1975@gmail.com>
2023-03-31 15:52:00 +0300

    Add linters error fix

--- src/components/TaskReducer.jsx
+++ src/components/TaskReducer.jsx
@@ -1,21 +1,19 @@
 const taskReducer = (tasks, action) => {
   switch (action.type) {
-    case "added_task": {
+    case 'added_task': {
       return tasks.concat({
         id: action.id,
         text: action.text,
         done: false,
       });
     }
-    case "toggle_task": {
-      return tasks.map((t) => {});
+    case 'toggle_task': {
+      return tasks.map((t) => (t.id === action.task.id ? action.task : t));
     }
-    case "changed_task": {
-      return tasks.map((t) => {
-        return t.id === action.task.id ? action.task : t;
-      });
+    case 'changed_task': {
+      return tasks.map((t) => (t.id === action.task.id ? action.task : t));
     }
-    case "deleted_task": {
+    case 'deleted_task': {
       return tasks.filter((t) => t.id !== action.id);
     }
 
